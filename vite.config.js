1e1ef4dabd3db4b8dd2dec57841bca6e0fd94f73
Abhijith-EchoFox <abhijith@echofoxdesign.com>
2025-06-25 10:33:27 +0530

    Changed vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production'
-    ? '/Echofox-TextEffect/'
+    ? '/fox_demo/'
     : '/',
 });
