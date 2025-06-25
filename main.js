4caa32fa406c7d257d44b98f7242510cd3a91d19
Abhijith-EchoFox <abhijith@echofoxdesign.com>
2025-06-25 13:23:30 +0530

    changed background

--- main.js
+++ main.js
@@ -199,7 +199,7 @@ const CustomAnaglyphShader = {
             vec2 shift = vec2(0.005 * intensity, 0.0);
             vec4 colorL = texture(tDiffuse, vUv - shift); // Sample for the left eye (red channel)
             vec4 colorR = texture(tDiffuse, vUv + shift); // Sample for the right eye (green/blue channels)
-            gl_FragColor = vec4(colorL.r, colorR.g, colorR.b,0);
+            gl_FragColor = vec4(colorL.r, colorR.g, colorR.b,0.3);
         }
     `
 };
